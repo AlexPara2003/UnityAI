f64b47ec2835568209775d57936d4efea9e36a22
AlexPara2003 <apara@student.neumont.edu>
2023-01-11 21:30:06 -0700

    Added moving AI

---
+++ Assets/Scripts/AutonomousAgent/AutonomousAgent.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class AutonomousAgent : Agent
+{
+    void Update()
+    {
+        var gameObjects = perception.GetGameObjects();
+        foreach (var gameObject in gameObjects)
+        {
+            Debug.DrawLine(transform.position, gameObject.transform.position);
+        }
+
+        if (gameObjects.Length > 0)
+        {
+            movement.ApplyForce(Steering.Seek(this, gameObjects[0]) * 0);
+            movement.ApplyForce(Steering.Flee(this, gameObjects[0]) * 1);
+        }
+        transform.position = Utilities.Wrap(transform.position, new Vector3(-10, -10, -10), new Vector3(10, 10, 10));
+    }
+}
